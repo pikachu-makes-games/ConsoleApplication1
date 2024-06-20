d1f408b9ae55a7230eb2f020f654241764151bf3
Jordan Grant <grantjordan191@gmail.com>
2024-06-20 18:42:35 -0400

    Add project files.

---
+++ ConsoleApplication1/BitFormatter.h
@@ -0,0 +1,12 @@
+#pragma once
+class BitFormatter
+{
+private:
+	int bufferi8[];
+public:
+	int formati8(int i8) {
+
+		return 0;
+	}
+};
+
---
+++ ConsoleApplication1/Buffer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace std;
+
+class Buffer
+{
+private:
+	int buffer[1028];
+
+public:
+
+};
+
